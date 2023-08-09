fd40a29eafa2a6c15e715c5bc8751556c0c23595
Andrew Mauney <andrew.e.mauney@gmail.com>
2023-08-09 15:42:21 -0400

    Update convert_to_chd.sh

--- convert_to_chd.sh
+++ convert_to_chd.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# requires rom-tools and/or chdman
+
 echo "Will proccess files on directory: $1"
 cd $1
 for i in */*.cue; do chdman createcd -i "$i" -o "${i%.*}.chd"; done
+for i in *.iso; do chdman createcd -i "$i" -o "${i%.*}.chd"; done
 for i in */*.chd; do echo "${i%.*}.chd" > "${i%.*}.m3u"; done
 for i in */*.bin; do rm "${i%.*}.bin"; done
 for i in */*.cue; do rm "${i%.*}.cue"; done
+for i in *.iso; do rm "${i%.*}.iso"; done
 for i in */*.m3u; do mv "${i%.*}.m3u" ./; done
 echo "All done"
